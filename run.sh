a8c4c3f26cdaa079e6d56b5cb7815835e10e1162
chLi <565353780@qq.com>
2026-01-05 21:44:55 +0800

    simplify running command

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 cd ../UltraShape-1.0
 
+DATA_FOLDER=$HOME/chLi/Dataset/MM/Match/longren/
+
 PYTHONPATH=$PYTHONPATH:$(pwd) \
   python scripts/infer_dit_refine.py \
   --ckpt $HOME/chLi/Model/UltraShape/ultrashape_v1.pt \
-  --image $HOME/chLi/Dataset/GS/haizei_1/input/image_4.png \
-  --mesh $HOME/chLi/Dataset/GS/haizei_1/mmv2/image_4.glb \
-  --config configs/infer_dit_refine.yaml
+  --image $DATA_FOLDER/longren.png \
+  --mesh $DATA_FOLDER/longren_stage1.glb \
+  --config configs/infer_dit_refine.yaml \
+  --output_dir $DATA_FOLDER
